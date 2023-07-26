749c99fc2e50c3c1602b40650802e788996e1fa5
codextra <insideabucampus@gmail.com>
2023-07-26 18:42:44 +0100

    testimonial changed

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -3,8 +3,15 @@ import React from 'react'
 function Testimonial() {
   return (
     <section className="section service-sec">
-        <p className="text">what do you need?</p>
-        <h1>Our Services</h1>
+        <p className="text">testimonials</p>
+        <h1>What Our Past Client Says</h1>
+        <div className="testimonials">
+            <div className="testtimonial">
+                <div className="rating"></div>
+                <div className="text"></div>
+                <div className="profile"></div>
+            </div>
+        </div>
     </section>
   )
 }
